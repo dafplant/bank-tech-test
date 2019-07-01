df27062f188365e8febaeac4566f63f5f2fdab34
Chris <chris.daf@gmail.com>
2019-07-01 13:50:26 +0100

    added tests and functionality for adding and reducing balance

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -1,9 +1,18 @@
 describe("bankAccount", function() {
-    account = new bankAccount();
+    var account = new bankAccount();
 
         it("can initialize a bank account with 0", function() {
             expect(account.balance).toBe(0);
         });
+        
+        it("can add 1000 to the balance", function() {
+            account.addToBalance(1000);
+            expect(account.balance).toBe(1000);
+        });
 
+        it("can reduce the balance by 500", function() {
+            account.reduceBalance(500);
+            expect(account.balance).toBe(500);
+        });
         
 });
